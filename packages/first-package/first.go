494d6d7f507705a5c400b185e66e54565dc7a714
Luiz Rioja <lulyrioja@gmail.com>
2024-06-04 20:23:48 -0300

    feat: adds learnings about packages

---
+++ packages/first-package/first.go
@@ -0,0 +1,8 @@
+package firstPackage
+
+import "fmt"
+
+func Write() {
+	fmt.Println("Writing from first.go file in firstPackage folder")
+	write()
+}
---
+++ packages/first-package/second.go
@@ -0,0 +1,7 @@
+package firstPackage
+
+import "fmt"
+
+func write() {
+	fmt.Println("Writing from second.go file in firstPackage folder")
+}
